d0743c4beda8afed0d9120977ffbb7b22fc9d9a2
Maya BuserDe <maya.buserde@banno.com>
2025-11-07 12:12:17 -0500

    update jh-core SD scripts to reuse spec

--- packages/jh-core/lib/formats/format.js
+++ packages/jh-core/lib/formats/format.js
@@ -6,17 +6,9 @@ const cssFormatter = require('style-dictionary/lib/common/formats.js')[
 function fileHeader(showFileHeader) {
   if (showFileHeader) {
     return `/**
- * Copyright 2025 Jack Henry.
+ * SPDX-FileCopyrightText: 2025 Jack Henry
+ *
  * SPDX-License-Identifier: Apache-2.0
- * * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * * http://www.apache.org/licenses/LICENSE-2.0
- * * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  *
  * Do not edit directly
  * Generated on ${new Date().toUTCString()}

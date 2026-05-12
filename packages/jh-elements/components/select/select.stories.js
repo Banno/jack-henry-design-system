264bc1c43f605fff13401fbf4b8603787760b65f
Maya BuserDe <mbuserde@jackhenry.com>
2026-05-12 10:53:36 -0400

    add back jh-datasets

--- packages/jh-elements/components/select/select.stories.js
+++ packages/jh-elements/components/select/select.stories.js
@@ -12,9 +12,9 @@ import "@jack-henry/jh-icons/icons-wc/icon-piggy-bank.js";
 import "@jack-henry/jh-icons/icons-wc/icon-arrow-up-small.js";
 import "@jack-henry/jh-icons/icons-wc/icon-arrow-down-small.js";
 import "../button/button.js";
-// import { US_STATES_FLAT } from '@jack-henry/jh-datasets/datasets/us-states-flat.js';
-// import { US_STATES_GROUPED } from '@jack-henry/jh-datasets/datasets/us-states-grouped.js';
-// import { manageDataset } from '@jack-henry/jh-datasets/utils/manageDataset.js';
+import { US_STATES_FLAT } from '@jack-henry/jh-datasets/datasets/us-states-flat.js';
+import { US_STATES_GROUPED } from '@jack-henry/jh-datasets/datasets/us-states-grouped.js';
+import { manageDataset } from '@jack-henry/jh-datasets/utils/manageDataset.js';
 
 const testOptions = [
   { groupLabel: "Account types", groupValues: [
@@ -245,36 +245,36 @@ Playground.parameters = {
   styles: storyStyles,
 };
 
-// export const Datasets = { render: (args) => {
-//   const customizedData = manageDataset({
-//     dataset: US_STATES_FLAT,
-//     initialValue: null,
-//     disabledItems: ['AK', 'HI', 'PR', 'VI', 'GU', 'AS'],
-//     emptyLabel: 'Select your state...',
-//   });
+export const Datasets = { render: (args) => {
+  const customizedData = manageDataset({
+    dataset: US_STATES_FLAT,
+    initialValue: null,
+    disabledItems: ['AK', 'HI', 'PR', 'VI', 'GU', 'AS'],
+    emptyLabel: 'Select your state...',
+  });
 
-//   return html`
-//     <div class="select-container">
-//       <jh-select label="US States (flat)" .options=${US_STATES_FLAT}></jh-select>
-//     </div>
-//     <div class="select-container">
-//       <jh-select label="US States (grouped)" .options=${US_STATES_GROUPED}></jh-select>
-//     </div>
+  return html`
+    <div class="select-container">
+      <jh-select label="US States (flat)" .options=${US_STATES_FLAT}></jh-select>
+    </div>
+    <div class="select-container">
+      <jh-select label="US States (grouped)" .options=${US_STATES_GROUPED}></jh-select>
+    </div>
 
-//     <h3></h3>
-//     <div class="select-container">
-//       <jh-select label="US states customized" helper-text="Uses manageDataset to set initial value, disabled items, empty label" .options=${customizedData}></jh-select>
-//     </div>
-//   `;
-// }};
+    <h3></h3>
+    <div class="select-container">
+      <jh-select label="US states customized" helper-text="Uses manageDataset to set initial value, disabled items, empty label" .options=${customizedData}></jh-select>
+    </div>
+  `;
+}};
 
-// Datasets.argTypes = {
-//   ...disableControls,
-// };
+Datasets.argTypes = {
+  ...disableControls,
+};
 
-// Datasets.parameters = {
-//   styles: storyStyles,
-// };
+Datasets.parameters = {
+  styles: storyStyles,
+};
 
 export const Empty = { render: (args) => html`
   <div class="select-container">

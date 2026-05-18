d50d127db0ac54208cf9e442f2933fa44dea378e
Audrey Bissiere-Grote <abissieregrote@jackhenry.com>
2026-05-18 15:59:13 -0400

    adds vite mdx bug fix

--- packages/jh-ui/.storybook/main.js
+++ packages/jh-ui/.storybook/main.js
@@ -10,13 +10,28 @@ const config = {
     'storybook-addon-tag-badges',
     '@storybook/addon-docs'
   ],
-
   framework: {
     name: '@storybook/web-components-vite',
     options: {}
   },
+  staticDirs: ['./public'],
+  // Vite fix for the file:// import issue when using MDX stories in Storybook
+  viteFinal: async (config) => {
+    if (!config.plugins) config.plugins = [];
 
-  staticDirs: ['./public']
+    config.plugins.push({
+      name: 'fix-mdx-file-urls',
+      enforce: 'pre',
+      resolveId(source) {
+        if (source.startsWith('file://')) {
+          return source.replace(/^file:\/\//, '').replace(/^\/([A-Za-z]:)/, '$1');
+        }
+        return null;
+      },
+    });
+
+    return config;
+  },
 };
 
 export default config;

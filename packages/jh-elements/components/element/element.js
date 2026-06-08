e92dfe5e456a7a5a150dbab5264ef47a8a6583fb
Audrey Bissiere-Grote <abissieregrote@jackhenry.com>
2026-06-08 13:24:28 -0400

    remove element registration

--- packages/jh-elements/components/element/element.js
+++ packages/jh-elements/components/element/element.js
@@ -7,10 +7,7 @@
 import { LitElement } from 'lit';
 
 let id = 0;
-/**
- * Element
- * @customElement jh-element
- */
+
 export class JhElement extends LitElement {
   /** @type {ElementInternals} */
   #internals;

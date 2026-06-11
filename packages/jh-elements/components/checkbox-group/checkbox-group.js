48ef1b163b614aefd929f9bc2b087739ce4e320e
Audrey Bissiere-Grote <abissieregrote@jackhenry.com>
2026-06-11 12:06:28 -0400

    removes unused id

--- packages/jh-elements/components/checkbox-group/checkbox-group.js
+++ packages/jh-elements/components/checkbox-group/checkbox-group.js
@@ -6,7 +6,6 @@ import { css, html } from 'lit';
 import { JhElement } from '../element/element.js';
 import { ifDefined } from 'lit/directives/if-defined.js';
 
-let id = 0;
 /**
  *
  * @cssprop --jh-checkbox-group-label-color-text - The label text color. Defaults to `--jh-color-content-primary-enabled`.

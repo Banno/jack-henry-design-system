50f77c0c833070f4539399bdc50e6e4c04bb6cb6
Audrey Bissiere-Grote <abissieregrote@jackhenry.com>
2026-06-30 15:26:50 -0400

    adds currency datasets

---
+++ packages/jh-datasets/datasets/currency-codes.js
@@ -0,0 +1,16 @@
+/**
+* SPDX-FileCopyrightText: 2026 Jack Henry
+*
+* SPDX-License-Identifier: Apache-2.0
+*/
+
+export const CURRENCY_CODES = [
+  { value: "USD" },
+  { value: "CAD" },
+  { value: "MXN" },
+  { value: "EUR" },
+  { value: "GBP" },
+  { value: "AUD" },
+  { value: "JPY" },
+  { value: "CNY" },
+];
---
+++ packages/jh-datasets/datasets/world-currencies.js
@@ -0,0 +1,16 @@
+/**
+* SPDX-FileCopyrightText: 2026 Jack Henry
+*
+* SPDX-License-Identifier: Apache-2.0
+*/
+
+export const WORLD_CURRENCIES = [
+  { label: "$", value: "USD" },
+  { label: "$", value: "CAD" },
+  { label: "$", value: "MXN" },
+  { label: "€", value: "EUR" },
+  { label: "£", value: "GBP" },
+  { label: "$", value: "AUD" },
+  { label: "¥", value: "JPY" },
+  { label: "¥", value: "CNY" },
+];

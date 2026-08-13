5fa5954be89701ae872598899c7a1edc2e00c6ce
Maya BuserDe <mbuserde@jackhenry.com>
2026-08-13 13:17:59 -0400

    fix icon template and regenerate wc-icons

--- packages/jh-icons/icons-wc/icon-wand-sparkles.js
+++ packages/jh-icons/icons-wc/icon-wand-sparkles.js
@@ -5,7 +5,7 @@
 */
 import {LitElement, css, html} from 'lit';
 
-export default class JhIconWandSparkle extends LitElement {
+export default class JhIconWandSparkles extends LitElement {
   /** @type {ElementInternals} */
   #internals;
 
@@ -91,4 +91,4 @@ export default class JhIconWandSparkle extends LitElement {
   }
 }
 
-customElements.define('jh-icon-wand-sparkles', JhIconWandSparkle);
+customElements.define('jh-icon-wand-sparkles', JhIconWandSparkles);
